bc6b5eed812fecb8e2d4f0daeaee74508130d077
ArthurBB <barbosarthur7@gmail.com>
2024-10-11 15:09:07 -0300

    Adiciona React

---
+++ Projeto_Solar_Coca_Cola/site-solar/src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+function App() {
+  return (
+    <ChakraProvider>
+     <div>
+      
+     </div>
+    
+    </ChakraProvider>
+  );
+}
+
+export default App;
